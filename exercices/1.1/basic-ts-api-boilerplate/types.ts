3c2ff5320b09275499aa3cc32c2f23bb06e39f06
Hamou <ryad.hamou@student.vinci.be>
2024-09-30 14:06:35 +0200

    exercice 1 fini

--- exercices/1.1/basic-ts-api-boilerplate/types.ts
+++ exercices/1.1/basic-ts-api-boilerplate/types.ts
@@ -1,3 +1,11 @@
+interface Film {
+    id: number;
+    title: string;
+    director: string;
+    duration: number;
+    budget?: number;
+    description?: string;
+    imageUrl?: string;
+}
 
-
-export type { };
+export type { Film };
