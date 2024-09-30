f194d18449840ae565cd1d35badcbef55e7b16e0
Hamou <ryad.hamou@student.vinci.be>
2024-09-30 12:20:17 +0200

    Premiere theorie

--- tutorials/back/api/basic/types.ts
+++ tutorials/back/api/basic/types.ts
@@ -1,14 +1,9 @@
-interface Pizza {
+interface Drink {
   id: number;
   title: string;
-  content: string;
+  image: string;
+  volume: number;
+  price: number;
 }
 
-interface PizzaToUpdate {
-  title?: string;
-  content?: string;
-}
-
-type NewPizza = Omit<Pizza, "id">;
-
-export type { Pizza, NewPizza, PizzaToUpdate };
+export type { Drink };
