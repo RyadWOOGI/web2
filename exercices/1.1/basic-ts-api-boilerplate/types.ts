24e6b4ed6a9688cbbcfd0f4cdcd78a4f3790acec
Hamou <ryad.hamou@student.vinci.be>
2024-09-30 12:30:13 +0200

    ex1

--- exercices/1.1/basic-ts-api-boilerplate/types.ts
+++ exercices/1.1/basic-ts-api-boilerplate/types.ts
@@ -1,14 +1,3 @@
-interface Pizza {
-  id: number;
-  title: string;
-  content: string;
-}
 
-interface PizzaToUpdate {
-  title?: string;
-  content?: string;
-}
 
-type NewPizza = Omit<Pizza, "id">;
-
-export type { Pizza, NewPizza, PizzaToUpdate };
+export type { };
