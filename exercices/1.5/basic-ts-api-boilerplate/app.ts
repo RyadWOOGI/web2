13468abe1c18f0dfb14e869bcc1c62dd60892186
Hamou <ryad.hamou@student.vinci.be>
2024-10-01 14:34:01 +0200

    1.5 first commit

---
+++ exercices/1.5/basic-ts-api-boilerplate/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+import filmsRouter from "./routes/films";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let count = 0; // compteur de requetes GET
+
+//Middleware compteur GET
+app.use((req, _res, next) => {
+  if (req.method === "GET") {
+    count++; //Incrémente le compteur
+    console.log(`GET counter: ${count}`);
+  }
+  next();
+});
+
+app.use("/films", filmsRouter);
+
+export default app;
