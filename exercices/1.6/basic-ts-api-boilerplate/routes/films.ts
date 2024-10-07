cff8dfd7009df6c1340c7a6eb09cc2032406336d
Hamou <ryad.hamou@student.vinci.be>
2024-10-07 13:45:32 +0200

    new: ex1.6.

--- exercices/1.6/basic-ts-api-boilerplate/routes/films.ts
+++ exercices/1.6/basic-ts-api-boilerplate/routes/films.ts
@@ -86,6 +86,11 @@ router.post("/", (req, res) => {
 
   const { title, director, duration, budget, description, imageUrl } = body as NewFilm;
 
+  const filmExist = films.find((film) => film.title === title && film.director === director);
+  if(filmExist) {
+    return res.sendStatus(409);
+  }
+
   const nextId = films.reduce((maxId, film) => (film.id > maxId ? film.id : maxId), 0) + 1;
 
   const NewFilm: Film = {
@@ -100,6 +105,124 @@ router.post("/", (req, res) => {
 
   films.push(NewFilm);
   return res.json(NewFilm);
+});
+
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = films.findIndex((film) => film.id === id);
+  if (index === -1) {
+    return res.sendStatus(404);
+  }
+  const deletedElements = films.splice(index, 1);
+  return res.json(deletedElements[0]);
+});
+
+router.patch("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const film = films.find((film) => film.id === id);
+  if (!film) {
+    return res.sendStatus(404);
+  } 
+
+  const body: unknown = req.body;
+
+  if (
+    !body ||
+    typeof body !== "object" || 
+    ("title" in body && 
+      (typeof body.title !== "string" || !body.title.trim())) || 
+    ("director" in body && 
+      (typeof body.director !== "string" || !body.director.trim())) || 
+    ("duration" in body && 
+      (typeof body.duration !== "number" || body.duration <= 0)) || 
+    ("budget" in body && 
+      (typeof body.budget !== "number" || body.budget <= 0)) || 
+    ("imageUrl" in body && (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
+  ) {
+    return res.sendStatus(400);
+  }
+
+  const  { title, director, duration, budget, description, imageUrl }: Partial<NewFilm> = body;
+
+  if (title) {
+    film.title = title;
+  }
+  if (director) {
+    film.director = director;
+  }
+  if (duration) {
+    film.duration = duration;
+  }
+  if (budget) {
+    film.budget = budget;
+  }
+  if (description) {
+    film.description = description;
+  }
+  if (imageUrl) {
+    film.imageUrl = imageUrl;
+  }
+
+  return res.json(film);
+});
+
+router.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const body: unknown = req.body;
+
+  if (
+    !body ||
+    typeof body !== "object" || 
+    ("title" in body && 
+      (typeof body.title !== "string" || !body.title.trim())) || 
+    ("director" in body && 
+      (typeof body.director !== "string" || !body.director.trim())) || 
+    ("duration" in body && 
+      (typeof body.duration !== "number" || body.duration <= 0)) || 
+    ("budget" in body && 
+      (typeof body.budget !== "number" || body.budget <= 0)) || 
+    ("imageUrl" in body && (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
+  ) {
+    return res.sendStatus(400);
+  }
+
+
+  const filmExist = films.find((film) => film.id === id);
+  const { title, director, duration, budget, description, imageUrl } = body as NewFilm;
+
+  if (filmExist) {
+  if (title && director && duration) {
+    filmExist.title = title;
+    filmExist.director = director;
+    filmExist.duration = duration;
+  }
+  if(budget) {
+    filmExist.budget = budget;
+  }
+  if(description) {
+    filmExist.description = description;
+  }
+  if(imageUrl) {
+    filmExist.imageUrl = imageUrl;
+  }
+  return res.json(filmExist);
+}
+
+const nextId = films.reduce((maxId, film) => (film.id > maxId ? film.id : maxId), 0) + 1;
+
+const NewFilm: Film = {
+  id: nextId,
+  title,
+  director,
+  duration,
+  budget,
+  description,
+  imageUrl
+};
+
+films.push(NewFilm);
+return res.json(NewFilm);
 });
 
 export default router;
