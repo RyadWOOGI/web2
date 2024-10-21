cd32ace29fb2351fbc6a6a74d2802893ad602c02
Hamou <ryad.hamou@student.vinci.be>
2024-10-21 19:40:01 +0200

    new:ex2.4.

--- exercices/2.1-2-3-4/2.1-2-3-4/src/App/index.tsx
+++ exercices/2.1-2-3-4/2.1-2-3-4/src/App/index.tsx
@@ -1,5 +1,7 @@
 import Cinema from "../Cinema"
 import PageTitle from "../PageTitle";
+import Footer from "../Footer";
+import Header from "../Header";
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
@@ -48,11 +50,18 @@ const App = () => {
 
   return (
     <div>
+      <Header urlLogo="https://plus.unsplash.com/premium_photo-1682125771198-f7cbed7cb868?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+      <h1>Tous sur les films</h1>
+      </Header>
       <PageTitle title={pageTitle} />
 
       <Cinema name={cinema1Name} movies={moviesCinema1} />
 
       <Cinema name={cinema2Name} movies={moviesCinema2} />
+
+      <Footer urlLogo="https://media.istockphoto.com/id/1202770152/fr/photo/bobine-de-film-disolement-sur-le-fond-jaune-lumineux-dans-les-couleurs-pastel.jpg?s=1024x1024&w=is&k=20&c=2yKBrC8oyimPdW-5IxFWN_zxFPVK3KWYL9OE2gVmVX4=">
+        <p>© 2021 UGC Cinémas</p>
+      </Footer>
     </div>
   );
 };
